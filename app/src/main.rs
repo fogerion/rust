f006982cf6ef26ab217ef0ece2299d677e00d5ab
fogerion <fogeryndbrdtl@gmail.com>
2025-06-18 11:40:52 +0900

    first commit

---
+++ app/src/main.rs
@@ -0,0 +1,29 @@
+//lesson 3 rust
+// fn main() {
+//     let mut num: f64 = 21.321321;
+//     let mut age: i64 = 21;
+//     //i64 it is integer and f64 it is float
+//     num = 25.123213;
+//     println!("Number is {}", num);
+// }
+
+// i8 i16 i32 i64 i128 isize
+// u8 u16 u32 u64 u128 usize - its plus integers not minus
+
+// fn main(){
+//     //let mut name = "Ivan";
+//     //let mut name: &str = "Ivan";
+//     let mut name = String::from("Denis");
+//     println!("Your name is {}", name);
+// }
+
+
+// lesson 4 rust
+
+fn main(){
+    //char
+    //bool
+
+    let symbol: char = 'q';
+    println!("{}",symbol);
+}
