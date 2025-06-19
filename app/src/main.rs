c255596c73e837dcf9c2e09f715f36a1811879bb
fogerion <fogeryndbrdtl@gmail.com>
2025-06-19 16:14:48 +0900

    lessons 9-12

--- app/src/main.rs
+++ app/src/main.rs
@@ -166,3 +166,216 @@
 //     }
 //     println!("{}", ready_num);
 // }
+
+//lesson 9 
+//enter
+
+// use std::io;
+// // io, i-input, o-output
+
+// fn main(){
+//     let mut name = String::new();
+
+//     println!("Whats ur name?");
+//     match io::stdin().read_line(&mut name){
+//         Ok(_)=> {
+//             println!("Hello, {}", name);
+//         },
+//         Err(e) => {
+//             println!("Program error -{}",e);
+//         }
+//     }
+// }
+
+
+// проект квадратных уравнений
+/*
+use std::io;
+
+fn main(){
+    //ax^2+bx+c=0
+    //D=b^2-4(ac)
+    
+
+    loop{
+        let mut a_str = String::new();
+        let mut b_str = String::new();
+        let mut c_str = String::new(); 
+        let mut quit_programm = String::new(); 
+        println!("решить квадратное уравнение");
+        println!("введите а");
+        match io::stdin().read_line(&mut a_str){
+            Ok(_) => {},
+            Err(e) => {
+                println!("Program error -{}",e);
+            }
+        }
+
+        let mut a: f64 = 0.0;
+        match a_str.trim().parse::<f64>() {  
+            Ok(num) => a = num,  
+            Err(e) => {
+                println!("Ошибка ввода коэффициента 'a': {}", e);
+                println!("'a': {}", a_str);
+                break;
+            },
+        };  
+
+        println!("введите b");
+        match io::stdin().read_line(&mut b_str){
+            Ok(_) => {},
+            Err(e) => {
+                println!("Program error -{}",e);
+            }
+        }
+        let mut b: f64 = 0.0;
+        match b_str.trim().parse::<f64>() {  
+            Ok(num) => b = num,  
+            Err(e) => {
+                println!("Ошибка ввода коэффициента 'b': {}", e);
+                println!("'b': {}", b_str);
+                break;  
+            },
+        };  
+
+        println!("введите c");
+        match io::stdin().read_line(&mut c_str){
+            Ok(_) => {},
+            Err(e) => {
+                println!("Program error -{}",e);
+            }
+        }
+        let mut c: f64 = 0.0;
+        match c_str.trim().parse::<f64>() {  
+            Ok(num) => c = num,  
+            Err(e) => {
+                println!("Ошибка ввода коэффициента 'c': {}", e);
+                println!("'c': {}", c_str);
+                break;  
+            },
+        };  
+      
+
+        let d: f64 = b*b - 4.0*(a*c);
+
+        if d>0.0 {
+            let x1 = ((-b)+ d.sqrt())/(2.0*a);
+            let x2 = ((-b)- d.sqrt())/(2.0*a);
+
+            println!("x1={}, x2={}", x1, x2);
+        }
+        else if d==0.0{
+            let x = -b/(2.0*a);
+
+            println!("x={}",x);
+        }
+        else {
+            println!("net kornei");
+        }
+
+        println!("if u want to quit enter quit:");
+        match io::stdin().read_line(&mut quit_programm){
+            Ok(_) => {},
+            Err(e) => {
+                println!("Program error -{}",e);
+            }
+        }
+
+        
+        if quit_programm.trim() == "quit"{
+            break;
+        }
+    }
+
+}
+
+*/
+
+
+//lesson 10
+//Массивы
+/*
+fn main(){
+    
+    // let mut array:[i64; 4] = [1, 2, 3, 4];
+
+    // println!("{:?}", array[1]);
+    // array[1] = 5;
+    
+    // println!("{:?}", array[1]);
+
+    // let arr = [2; 50];
+    // println!("{:?}", arr);
+    let array = [1,2,3,4,5,8,67,7];
+
+    // for i in array.iter(){
+    //     println!("{}",i);
+    // }
+
+    println!("{}", array.len());
+
+    // for i in 0..array.len(){
+    //     println!("{}", array[i]);
+    // }
+
+    // let mut i = 0;
+    // while i < array.len(){
+    //     println!("{}", array[i]);
+    //     i+=1;
+    // }
+
+    for i in array.iter(){
+        if i%2==0{
+            println!("{}",i);
+        }
+
+    }
+
+
+}*/
+
+
+//lesson 11 rust
+// const
+// const C: i64 = 5;
+// fn main(){
+//     let c = 5;
+//     println!("{}", C)
+// }
+// fn sum(a: i8, b: i8) -> i8{
+//     a+b
+// }
+// fn main(){
+//     let c = sum(2,3);
+//     println!("{}", c);
+// }
+
+
+
+//lesson 12 rust
+//кортежи
+// fn main(){
+//     // let tuple = (12,34.6, "Lol");
+//     // println!("{:?}", tuple);
+
+//     let pupil = (String::from("Denis"), 11);
+
+    
+
+//     let name_ = pupil.0;
+//     let grade_ = pupil.1;
+//     println!("{}", name_);
+//     println!("{}", grade_);
+
+//     // let (name, grade) = pupil;
+//     // println!("{}", name);
+//     // println!("{}", grade);
+// }
+
+
+//lesson 13 rust
+//functions
+
+fn main(){
+    
+}
