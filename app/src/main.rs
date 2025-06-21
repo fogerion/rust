77580350457f49d86e94fb4472b38051eb9e0595
fogerion <fogeryndbrdtl@gmail.com>
2025-06-21 13:29:39 +0900

    finished lesson 14

--- app/src/main.rs
+++ app/src/main.rs
@@ -57,11 +57,94 @@ mod lessons{
 //     s
 //  }
 
+
+// использование ссылок
+// fn main(){
+//     let s1 = String::from("TEXT");
+//     println!("{}", calc(&s1));
+
+//     println!("{}", s1);
+// }
+
+// fn calc(s: &String) -> usize {
+//     s.len()
+// }
+
+
+// использование ссылок с изменениями переменной
+// fn main(){
+//     let mut s = String::from("Text");
+//     println!("{}", s);
+//     add(&mut s);
+//     println!("{}", s);
+// }
+
+// fn add(s: &mut String){
+//     s.push_str(" IS AWESOME")
+// }
+// fn main(){
+//     let mut name = String::new();
+
+//     std::io::stdin().read_line(&mut name).expect("Err");
+
+//     println!("My name is {}", name);
+// }
+
+
+// ограничение на изменяемые ссылки, не больше одного на переменного
+// fn main(){
+//     let mut s = String::from("Text");
+
+//     let refe = &mut s;
+//     let refe1 = &mut s;
+
+//     println!("{}, {}", refe, refe1);
+// }
+
+// из-за владения
+// и области видимости
+// можно создавать две изменяемые ссылки на переменную
+// fn main(){
+//     let mut s = String::from("Text");
+
+//     let x = true;
+
+//     if x == true {
+//         let ref1=&mut s;
+//         println!("{}", ref1);
+//     }
+
+//     let ref2 = &mut s;
+//     println!("{}", ref2);
+//     // а так субобласть владения надо стоят впереди
+// }
+
+
+// висячие ссылки
+// если ретурнить ссылку на другое владение
+// то ретурниться пустота, а в расте ошибка
+// fn main(){
+//     let n = 65;
+//     let s = ret();
+//     println!("{}", s);
+// }
+
+// fn ret() -> String{
+//     let str = String::from("text");
+//     str
+// }
+
+//срез
 fn main(){
-    let s1 = String::from("TEXT");
+    let str = String::from("TEXT HERE");
 
-}
+    let slice = &str[0..4];
+    // [..4] = [0..4]
+    // [0..9] or [..] all
+    println!("{}", slice);
 
-fn calc(s: String) -> usize {
-    s.len()
+    let arr = [1,2,3,4,5];
+    let slice = &arr[1..3];
+
+    println!("{:?}", slice);
 }
